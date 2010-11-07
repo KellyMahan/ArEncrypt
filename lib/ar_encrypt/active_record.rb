ef54a71cc462fac26f3f66ec0f5302e93c938b37
Kelly Mahan <kelly@digimedia.com>
2010-11-06 22:55:03 -0500

    added other params for finds

--- lib/ar_encrypt/active_record.rb
+++ lib/ar_encrypt/active_record.rb
@@ -14,10 +14,32 @@ module ActiveRecord
           return eval("self.#{attr_name} == ArEncrypt.encrypt(\"#{eval("match_str")}\")")
         end
         class_eval do
-          eval("def self.#{attr_name}_find(str); find_by_#{attr_name}(ArEncrypt.encrypt(str)); end;")
+          eval("def self.#{attr_name}_find(str, options = {})
+          conditions_array = ['1 = 1']
+          options.each_pair do |key, value|
+            conditions_array << \"\#{key} = '\#{value}'\"
+          end
+          if conditions_array.length > 0
+            conditions_str = \" and \#{conditions_array.join(' and ')}\"
+          else
+            conditions_str = ''
+          end
+          find(:first, :conditions=>[\"#{attr_name} = ? \#{conditions_str}\", ArEncrypt.encrypt(str)])
+          end")
         end
         class_eval do
-          eval("def self.#{attr_name}_find_all(str); find(:all, :conditions=>[\"#{attr_name} = ?\", ArEncrypt.encrypt(str)]); end;")
+          eval("def self.#{attr_name}_find_all(str, options = {})
+          conditions_array = []
+          options.each_pair do |key, value|
+            conditions_array << \"\#{key} = '\#{value}'\"
+          end
+          if conditions_array.length > 0
+            conditions_str = \" and \#{conditions_array.join(' and ')}\"
+          else
+            conditions_str = ''
+          end
+          find(:all, :conditions=>[\"#{attr_name} = ? \#{conditions_str}\", ArEncrypt.encrypt(str)])
+          end")
         end
         
         before_save "encrypt_#{attr_name}"
